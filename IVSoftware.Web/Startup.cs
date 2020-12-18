9eedf3dd18569b0756cbfa7d91447e45735f44fb
Sergio De La Hoz <sdelahoz@nimocom.com>
2020-12-17 19:39:42 -0500

    Se limpia Startup

--- IVSoftware.Web/Startup.cs
+++ IVSoftware.Web/Startup.cs
@@ -1,22 +1,18 @@
+using IVSoftware.Web.Data;
+using IVSoftware.Web.Helpers;
+using IVSoftware.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using IVSoftware.Web.Models;
-using IVSoftware.Web.Data;
-using Microsoft.CodeAnalysis.FlowAnalysis;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.CodeAnalysis.CodeFixes;
-using IVSoftware.Web.Helpers;
 
 namespace IVSoftware.Web
 {
     public class Startup
     {
-        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,7 +20,6 @@ namespace IVSoftware.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddIdentity<Persona, IdentityRole>(cfg =>
@@ -39,13 +34,6 @@ namespace IVSoftware.Web
             })
             .AddEntityFrameworkStores<IVSoftwareContext>();
 
-            //services.AddCors(o => o.AddPolicy(MyAllowSpecificOrigins, builder =>
-            //{
-            //    builder.AllowAnyOrigin()
-            //           .AllowAnyMethod()
-            //           .AllowAnyHeader();
-            //}));
-
             services.AddControllersWithViews();
 
             services.AddDbContext<IVSoftwareContext>(
@@ -56,7 +44,6 @@ namespace IVSoftware.Web
             services.AddScoped<IUserHelper, UserHelper>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -66,27 +53,20 @@ namespace IVSoftware.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthentication();
-            //app.UseCookiePolicy();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
-           
         }
     }
 }
