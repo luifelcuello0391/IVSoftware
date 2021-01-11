5fd62a1d9061701e87c21303edf0b94531fb4cbb
Sergio De La hoz <sergiodlz@outlook.com>
2021-01-11 18:23:15 -0500

    Crud Roles y responsabilidades

--- IVSoftware.Data/Models/JobRole.cs
+++ IVSoftware.Data/Models/JobRole.cs
@@ -1,10 +1,14 @@
 ﻿using IVSoftware.Data.Models.Core;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace IVSoftware.Data.Models
 {
     public class JobRole : BaseModel<int>
     {
+        [DisplayName("Nombre")]
+        [Required(ErrorMessage = "El {0} es requerido.")]
         public string Name { get; set; }
 
         public virtual ICollection<PersonJobRole> PeopleJobRole { get; set; }
