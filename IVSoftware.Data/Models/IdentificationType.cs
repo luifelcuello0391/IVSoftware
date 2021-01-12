6c951b9f2f1e091315fd8b905ed8b4405d1adf39
Sergio De La hoz <sergiodlz@outlook.com>
2021-01-11 19:06:31 -0500

    CRUD Tipo de Identificación

--- IVSoftware.Data/Models/IdentificationType.cs
+++ IVSoftware.Data/Models/IdentificationType.cs
@@ -1,12 +1,17 @@
 ﻿using IVSoftware.Data.Models.Core;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace IVSoftware.Data.Models
 {
     public class IdentificationType : BaseModel<int>
     {
+        [DisplayName("Nombre")]
+        [Required(ErrorMessage = "El {0} es requerido.")]
         public string Name { get; set; }
 
+        [DisplayName("Código")]
         public string Code { get; set; }
 
         public virtual ICollection<Person> People { get; set; }
