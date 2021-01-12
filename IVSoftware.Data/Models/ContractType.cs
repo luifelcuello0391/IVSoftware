b252c86ce413f3d62df71feb89267a2efc0ea944
Sergio De La hoz <sergiodlz@outlook.com>
2021-01-11 19:31:14 -0500

    Se agregan validaciones al modelo de contractType

--- IVSoftware.Data/Models/ContractType.cs
+++ IVSoftware.Data/Models/ContractType.cs
@@ -1,10 +1,14 @@
 ﻿using IVSoftware.Data.Models.Core;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace IVSoftware.Data.Models
 {
     public class ContractType : BaseModel<int>
     {
+        [DisplayName("Nombre")]
+        [Required(ErrorMessage = "El {0} es requerido.")]
         public string Name { get; set; }
 
         public virtual ICollection<Person> People { get; set; }
