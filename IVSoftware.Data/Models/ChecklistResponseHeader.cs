fa32c68828d40955d26eff660401f045353b6581
Luis Felipe Cuello Ayala <luifelcuello@gmail.com>
2021-01-25 21:44:55 -0500

    - Se agrega la visualización de una lista de chequeo diligenciada.

--- IVSoftware.Data/Models/ChecklistResponseHeader.cs
+++ IVSoftware.Data/Models/ChecklistResponseHeader.cs
@@ -29,5 +29,62 @@ namespace IVSoftware.Data.Models
         public virtual Person ValidatedBy { get; set; }
         public virtual ICollection<CheckListResponseDetail> Details { get; set; }
 
+        public string GetResponse (int? sectionId, int questionId, int questionType)
+        {
+            string response = "N/A";
+
+            if(Details != null && Details.Count > 0)
+            {
+                try
+                {
+                    CheckListResponseDetail detail = null;
+
+                    if (sectionId != null)
+                    {
+                        detail = Details.Where<CheckListResponseDetail>(x => x.SectionId == sectionId && x.QuestionId == questionId).FirstOrDefault();
+                    }
+                    else
+                    {
+                        detail = Details.Where<CheckListResponseDetail>(x => x.SectionId == null && x.QuestionId == questionId).FirstOrDefault();
+                    }                    
+                    
+                    if(detail != null)
+                    {
+                        response = detail.Response;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Error on GetResponse >> " + ex.ToString());
+                }
+            }
+
+            switch(questionType)
+            {
+                case 1: // Si-NO-N/A
+                    switch(response.Trim())
+                    {
+                        case "1":
+                            return "Si";
+
+                        case "2":
+                            return "No";
+
+                        default:
+                            return "N/A";
+                    }
+
+                default:
+                    if(response == null || (response.Equals("N/A")))
+                    {
+                        return "Sin respuesta";
+                    }
+                    else
+                    {
+                        return response;
+                    }
+            }
+        }
+
     }
 }
