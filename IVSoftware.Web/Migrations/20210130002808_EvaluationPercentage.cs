2069f3055debf17bb52481394b6485948e911ad1
Sergio De La hoz <sdelahoz@lean-tech.io>
2021-01-29 19:42:58 -0500

    Se agrega el campo PercentageToPass a la evaluación

---
+++ IVSoftware.Web/Migrations/20210130002808_EvaluationPercentage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IVSoftware.Web.Migrations
+{
+    public partial class EvaluationPercentage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PercentageToPass",
+                table: "Evaluation",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PercentageToPass",
+                table: "Evaluation");
+        }
+    }
+}
