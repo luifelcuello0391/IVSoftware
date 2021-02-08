50a2fedfe9dc6b91f2b51776b8751df07760c0b9
Sergio De La hoz <sdelahoz@lean-tech.io>
2021-02-07 20:00:31 -0500

    Se agrega campo y migración de asignación de evaluación

---
+++ IVSoftware.Web/Migrations/20210208004744_EvaluationAssignedDate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IVSoftware.Web.Migrations
+{
+    public partial class EvaluationAssignedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AssignedDate",
+                table: "PersonEvaluations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignedDate",
+                table: "PersonEvaluations");
+        }
+    }
+}
