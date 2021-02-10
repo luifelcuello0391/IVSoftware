77194c4fd6ba464ab0eced96755c750478037066
Sergio De La hoz <sdelahoz@lean-tech.io>
2021-02-10 17:03:57 -0500

    SP para validar el vencimiento de las evaluaciones

---
+++ IVSoftware.Web/InitialScripts/OverdueEvaluationsSP.sql
@@ -0,0 +1,24 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Sergio De La Hoz
+-- Create date: 2021/02/10
+-- Description:	Procedimiento encargado de marcar como reporbada las evaluaciones vencidas
+-- =============================================
+CREATE OR ALTER PROCEDURE OverdueEvaluations
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    UPDATE pe
+	SET IsApproved = 0,
+		Score = 0
+	FROM PersonEvaluations pe
+	INNER JOIN Evaluation e ON (pe.EvaluationId = E.Id)
+	WHERE pe.ResultJson IS NULL AND
+		  pe.IsApproved IS NULL AND
+		  e.EndDate <= GETDATE()
+END
+GO
